5ade7ea2ae319dc6fa2e06c66fea3647f07f5338
a12331181 <a12331181@gmail.com>
2021-06-28 18:41:59 +0800

    Add admin restaurant create form & post feature

--- routes/index.js
+++ routes/index.js
@@ -16,16 +16,21 @@ module.exports = (app,passport) => {
     }
     res.redirect('/signin')
   }
+  
   app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
   app.get('/restaurants', authenticated, restController.getRestaurants)
 
   app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
   app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
+  
+  app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
+  app.post('/admin/restaurants', authenticatedAdmin, adminController.postRestaurant)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
 
   app.get('/signin', userController.signInPage)
   app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+  
   app.get('/logout', userController.logout)
 }
