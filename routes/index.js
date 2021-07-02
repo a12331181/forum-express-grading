50bd97ed71750fd9b59d527ef3603a0fa42c9c38
a12331181 <a12331181@gmail.com>
2021-07-02 15:50:23 +0800

    add restaurants & users switch button

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,14 @@ const upload = multer({ dest: 'temp/' })
 
 module.exports = (app,passport) => {
   const authenticated = (req, res, next) => {
-    if (helpers.ensureAuthenticated()) {
+    if (req.isAuthenticated()) {
       return next()
     }
     res.redirect('/signin')
   }
   const authenticatedAdmin = (req, res, next) => {
-    if (helpers.ensureAuthenticated(req)) {
-      if (helpers.getUser(req).isAdmin) { return next() }
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) { return next() }
       return res.redirect('/')
     }
     res.redirect('/signin')
