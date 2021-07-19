7b848ba28f97e7edb45428e1ffabdb7a760a5cda
a12331181 <a12331181@gmail.com>
2021-07-19 19:16:52 +0800

    POST /api/admin/restaurants

--- routes/apis.js
+++ routes/apis.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
 
 const adminController = require('../controllers/api/adminController.js')
 const categoryController = require('../controllers/api/categoryController.js')
@@ -7,8 +9,10 @@ const categoryController = require('../controllers/api/categoryController.js')
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 
-router.get('/admin/categories', categoryController.getCategories)
+router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
 
 router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
+
+router.get('/admin/categories', categoryController.getCategories)
 
 module.exports = router
