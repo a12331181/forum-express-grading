cc4a602415127dbf6566b1825109f3e98d2417be
a12331181 <a12331181@gmail.com>
2021-07-23 14:58:13 +0800

    add category API

--- controllers/api/categoryController.js
+++ controllers/api/categoryController.js
@@ -1,11 +1,24 @@
 const db = require('../../models')
 const Category = db.Category
-
 const categoryService = require('../../services/categoryService.js')
-
 let categoryController = {
   getCategories: (req, res) => {
     categoryService.getCategories(req, res, (data) => {
+      return res.json(data)
+    })
+  },
+  postCategory: (req, res) => {
+    categoryService.postCategory(req, res, (data) => {
+      return res.json(data)
+    })
+  },
+  putCategory: (req, res) => {
+    categoryService.putCategory(req, res, (data) => {
+      return res.json(data)
+    })
+  },
+  deleteCategory: (req, res) => {
+    categoryService.deleteCategory(req, res, (data) => {
       return res.json(data)
     })
   }
